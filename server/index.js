a5b28a387a9cbba3b6ca890dbef662177ecd7503
Venkat-Nithin <venkatnithinm@gmail.com>
2025-07-10 03:35:56 +0530

    Fixed cors

--- server/index.js
+++ server/index.js
@@ -9,7 +9,12 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://imagen-ai-psi.vercel.app',
+  methods: ['GET', 'POST'],
+  credentials: true,
+}));
+
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes);
